3b4132c14d3adcd64d592f1d46a1793f97dfdabd
tanmayymishra <tanmaymishra.cs16@mpgi.edu.in>
2023-11-08 18:23:39 +0530

    bike image fix in edit app

--- src/components/appBuilderPage/forms/AccountForm.jsx
+++ src/components/appBuilderPage/forms/AccountForm.jsx
@@ -1,26 +1,51 @@
-import React,{useEffect, useContext} from "react";
+import React, { useEffect, useContext } from "react";
 import { Grid, Typography } from "@mui/material";
 import { InputField } from "../../formFields";
-import { AppDetailsContext ,ColorContext} from "../../../context/contexts";
+import {
+  AppDetailsContext,
+  ColorContext,
+  DefaultContext,
+} from "../../../context/contexts";
 import { useFormikContext } from "formik";
 
 const AccountForm = (props) => {
-  const {values}= useFormikContext()
-  const { colors,setColors } = useContext(ColorContext);
+  const { selectForm } = props;
+  console.log(selectForm, "selectttttt");
+  const { values } = useFormikContext();
+  const { colors, setColors } = useContext(ColorContext);
+  const { defaultBike, setDefaultBike } = useContext(DefaultContext);
+  useEffect(() => {
+   if(selectForm==="edit"){
+    let models=[]
+    models=values.deviceInfo.bikeModels.map((item, index)=>{
+     return {
+       value:item.modelName,
+       label:item.modelName
+     }
+   })
+   values.deviceInfo.default && (
+     values.deviceInfo.bikeModels.map((bike, index)=>{
+       if(bike.modelName=== values.deviceInfo.default){
+         return setDefaultBike(bike)
+       }
+       // else(setFieldValue(values.deviceInfo.default,""))
+     })
+   )
+   }
+  }, []);
 
-  const {appDetails, setAppDetails} = useContext(AppDetailsContext)
-  useEffect(()=>{
-    setColors(
-      {primary: values.appTheme.primaryColor,
-        secondary:values.appTheme.secondaryColor,
-        accent:values.appTheme.accentColor
-      }
-    )
+  const { appDetails, setAppDetails } = useContext(AppDetailsContext);
+  useEffect(() => {
+    setColors({
+      primary: values.appTheme.primaryColor,
+      secondary: values.appTheme.secondaryColor,
+      accent: values.appTheme.accentColor,
+    });
     setAppDetails({
       ...appDetails,
-      brandName:values?.account?.brandName,
-    })
-  },[values?.account?.brandName])
+      brandName: values?.account?.brandName,
+    });
+  }, [values?.account?.brandName]);
 
   return (
     <React.Fragment>
@@ -31,7 +56,7 @@ const AccountForm = (props) => {
         <Grid item xs={12} sm={12}>
           <InputField name="account.appName" label="App Name" fullWidth />
         </Grid>
-       
+
         <Grid item xs={12} sm={12}>
           <InputField name="account.brandName" label="Brand Name" fullWidth />
         </Grid>
@@ -46,7 +71,12 @@ const AccountForm = (props) => {
           />
         </Grid>
         <Grid item xs={12} sm={12}>
-          <InputField name="account.serverURL" label="Server URL" fullWidth disabled/>
+          <InputField
+            name="account.serverURL"
+            label="Server URL"
+            fullWidth
+            disabled
+          />
         </Grid>
         <Grid item xs={12} sm={12}>
           <InputField
