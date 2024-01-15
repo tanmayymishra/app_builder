08a4be90b9c09d5228d7c28e9f877cb096786ac1
AmolKed123 <amol.kedar@thingsup.io>
2024-01-15 22:31:22 +0530

    splashscreen , screens & bikeParameters view key not delete condition added
    splashscreen , screens & bikeParameters view key not delete condition added

--- src/components/appBuilderPage/forms/FeaturesInfoParsers.js
+++ src/components/appBuilderPage/forms/FeaturesInfoParsers.js
@@ -5,23 +5,32 @@ export default function FeaturesInfoParsers(values) {
     for (let k in appFeatures) {
         if(k === "splashScreen"){
             for(let i in appFeatures["splashScreen"]){
+                if(i !== 'view'){
                 if(appFeatures["splashScreen"].hasOwnProperty(i) && appFeatures["splashScreen"][i] === false ){
+                    console.log("splash screen view",i);
                     delete appFeatures["splashScreen"][i];
                 }
+            }
             }
         }
         else if(k === "screens"){
             for(let i in appFeatures["screens"]){
+                if(i !== 'view'){
                 if(appFeatures["screens"].hasOwnProperty(i) && appFeatures["screens"][i] === false ){
+                    console.log("screens view",i);
                     delete appFeatures["screens"][i];
                 }
+            }
             }
         }
         else if(k === "bikeParameters"){
             for(let i in appFeatures["bikeParameters"]){
+                if(i !== 'view'){
                 if(appFeatures["bikeParameters"].hasOwnProperty(i) && appFeatures["bikeParameters"][i] === false ){
+                    console.log("bikeParameters view",i);
                     delete appFeatures["bikeParameters"][i];
                 }
+            }
             }
         }
         else if(k === "weather"){
