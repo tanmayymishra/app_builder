9d5ab6ab1dc57bc6d3c03f0e7995814c917f0a73
AmolKed123 <amol.kedar@thingsup.io>
2024-01-16 19:26:42 +0530

    url changed in config.json
    url changed in config.json

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
-//export let base_url = window.location.origin;
-export let base_url = "http://15.206.158.9:3001";
+export let base_url = window.location.origin;
+//export let base_url = "http://15.206.158.9:3001";
